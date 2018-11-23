4b19a378f13d60e19546ef171732cf71f1bc2293
kanekotic <alvarojosepl@gmail.com>
2018-11-23 10:26:12 +0100

    added missing imports for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
     input: "src/index.js",
